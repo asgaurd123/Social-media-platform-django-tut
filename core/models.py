1c5b4fadac8861430e6330b6e8662cb4dcae31c3
Lohith <sailohithkilli@gmail.com>
2022-05-10 17:39:24 +0530

    signup-page

--- core/models.py
+++ core/models.py
@@ -7,6 +7,8 @@ User=get_user_model()
 class Profile(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     id_user=models.IntegerField()
+    bio=models.TextField(blank=True)
     profileImg=models.ImageField(upload_to="profile_images", default="blank-profile-image.png")
+    location=models.CharField(max_length=100,blank=True)
     def __str__(self):
         return self.user.username
